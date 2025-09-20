15ea1b5fd73e4cdcddf65ee6fe6cc230a6cdd235
Alok Ranjan Thakur <a.thakurm007@gmail.com>
2025-09-20 02:14:32 +0000

    besided on rigt side of document summary there should be text to speech

--- src/components/app/summary-tab.tsx
+++ src/components/app/summary-tab.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { MarkdownRenderer } from './markdown-renderer';
+import { TTSButton } from './tts-button';
 
 type SummaryTabProps = {
   summary: string;
@@ -10,8 +11,13 @@ export function SummaryTab({ summary }: SummaryTabProps) {
   return (
     <Card className="shadow-lg">
       <CardHeader>
-        <CardTitle>Document Summary</CardTitle>
-        <CardDescription>A concise overview of the key points in your document.</CardDescription>
+        <div className="flex justify-between items-start">
+          <div>
+            <CardTitle>Document Summary</CardTitle>
+            <CardDescription>A concise overview of the key points in your document.</CardDescription>
+          </div>
+          <TTSButton textToSpeak={summary} />
+        </div>
       </CardHeader>
       <CardContent>
         <ScrollArea className="h-[400px] lg:h-[500px] pr-4">
